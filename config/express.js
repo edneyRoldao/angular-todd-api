ae5b5892b6a03d43860b533089cdb2e5c9bed3b0
Edy <edneyroldao@gmail.com>
2019-02-19 09:45:08 -0300

    adicionando config de cors que permite same origin policy

--- config/express.js
+++ config/express.js
@@ -9,5 +9,11 @@ let bodyParser = require('body-parser');
     app.use( bodyParser.urlencoded({extended: true}) );
     app.use( bodyParser.json() );
 
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     return app;
  };
